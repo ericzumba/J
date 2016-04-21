015d235481b3392c2f1d51a5e3de5bbc61a51836
Eric Torti <eric.torti@vivareal.com>
2016-04-21 06:16:50 -0300

    working nested

--- src/main/java/com/ericzumba/J.java
+++ src/main/java/com/ericzumba/J.java
@@ -6,26 +6,30 @@ import static java.util.stream.Collectors.joining;
 
 public class J {
 
-	public final String s;
+	private final String s;
 
 	private J(String k, String v) {
-		this.s = format("{\"%s\": \"%s\"}", k, v);
+		this.s = format("\"%s\": \"%s\"", k, v);
 	}
 	
 	private J(String k, long v) {
-		this.s = format("{\"%s\": %d}", k, v);
+		this.s = format("\"%s\": %d", k, v);
 	}
 
 	private J(String k, boolean v) {
-		this.s = format("{\"%s\": %b}", k, v);
+		this.s = format("\"%s\": %b", k, v);
 	}
 
 	private J(String k, double v) {
-		this.s = format("{\"%s\": %.2f}", k, v);
+		this.s = format("\"%s\": %.2f", k, v);
+	}
+	
+	public J(String k, J v) {
+		this.s = format("\"%s\": {%s}", k, v.s);
 	}
 	
 	public J(String k, J... v) {
-		this.s = format("{\"%s\": %s}", k, asList(v).stream().map((j) -> j.s).collect(joining(", ")));
+		this.s = format("\"%s\": {%s}", k, asList(v).stream().map((j) -> j.s).collect(joining(", ")));
 	}
 
 	public static J o(String k, String v) {
@@ -44,12 +48,16 @@ public class J {
 		return new J(k, v);
 	}
 	
+	public static J o(String k, J v) {
+		return new J(k, v);
+	}
+	
 	public static J o(String k, J... v) {
 		return new J(k, v);
 	}
 	
 	@Override
 	public String toString() {
-		return this.s;
+		return format("{%s}", s);
 	}
 }
--- src/test/java/com/ericzumba/JTest.java
+++ src/test/java/com/ericzumba/JTest.java
@@ -9,32 +9,32 @@ public class JTest {
 
 	@Test
 	public void string_object() throws Exception {
-		assertEquals("{\"a\": \"b\"}", o("a", "b").s);
+		assertEquals("{\"a\": \"b\"}", o("a", "b").toString());
 	}
 	
 	@Test
 	public void integer_object() throws Exception {
-		assertEquals("{\"a\": 1}", o("a", 1).s);
+		assertEquals("{\"a\": 1}", o("a", 1).toString());
 	}
 	
 	@Test
 	public void boolean_object() throws Exception {
-		assertEquals("{\"a\": true}", o("a", true).s);
+		assertEquals("{\"a\": true}", o("a", true).toString());
 	}
 	
 	@Test
 	public void float_object() throws Exception {
-		assertEquals("{\"a\": 1.23}", o("a", 1.23).s);
+		assertEquals("{\"a\": 1.23}", o("a", 1.23).toString());
 	}
 	
 	@Test
 	public void nested_object() throws Exception {
-		assertEquals("{\"a\": {\"b\": \"c\"}}", o("a", o("b", "c")).s);
+		assertEquals("{\"a\": {\"b\": \"c\"}}", o("a", o("b", "c")).toString());
 	}
 	
 	@Test
 	public void multiple_object() throws Exception {
-		assertEquals("{\"a\": {\"b\": \"c\", \"d\": \"d\" }}", J.o("a", o("b", "c"), o("d", "e")).s);
+		assertEquals("{\"a\": {\"b\": \"c\", \"d\": \"e\"}}", o("a", o("b", "c"), o("d", "e")).toString());
 	}
 	
 }
