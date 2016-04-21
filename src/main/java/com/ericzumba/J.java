9652e7c18592538fbe64a33105c1bba4f557995d
Eric Torti <eric.torti@vivareal.com>
2016-04-21 05:48:09 -0300

    nested

--- src/main/java/com/ericzumba/J.java
+++ src/main/java/com/ericzumba/J.java
@@ -4,20 +4,50 @@ import static java.lang.String.format;
 
 public class J {
 
-	public static String o(String k, String v) {
-		return format("{\"%s\": \"%s\"}", k, v);
+	public final String s;
+
+	private J(String k, String v) {
+		this.s = format("{\"%s\": \"%s\"}", k, v);
+	}
+	
+	private J(String k, long v) {
+		this.s = format("{\"%s\": %d}", k, v);
 	}
 
-	public static String o(String k, long v) {
-		return format("{\"%s\": %d}", k, v);
+	private J(String k, boolean v) {
+		this.s = format("{\"%s\": %b}", k, v);
 	}
 
-	public static String o(String k, boolean v) {
-		return format("{\"%s\": %b}", k, v);
+	private J(String k, double v) {
+		this.s = format("{\"%s\": %.2f}", k, v);
+	}
+	
+	public J(String k, J v) {
+		this.s = format("{\"%s\": %s}", k, v);
 	}
 
-	public static Object o(String k, double v) {
-		return format("{\"%s\": %.2f}", k, v);
+	public static J o(String k, String v) {
+		return new J(k, v);
 	}
-
+	
+	public static J o(String k, long v) {
+		return new J(k, v);
+	}
+	
+	public static J o(String k, boolean v) {
+		return new J(k, v);
+	}
+	
+	public static J o(String k, double v) {
+		return new J(k, v);
+	}
+	
+	public static J o(String k, J v) {
+		return new J(k, v);
+	}
+	
+	@Override
+	public String toString() {
+		return this.s;
+	}
 }
--- src/test/java/com/ericzumba/JTest.java
+++ src/test/java/com/ericzumba/JTest.java
@@ -9,23 +9,26 @@ public class JTest {
 
 	@Test
 	public void string_object() throws Exception {
-		assertEquals("{\"a\": \"b\"}", o("a", "b"));
+		assertEquals("{\"a\": \"b\"}", o("a", "b").s);
 	}
 	
 	@Test
 	public void integer_object() throws Exception {
-		assertEquals("{\"a\": 1}", o("a", 1));
+		assertEquals("{\"a\": 1}", o("a", 1).s);
 	}
 	
 	@Test
 	public void boolean_object() throws Exception {
-		assertEquals("{\"a\": true}", o("a", true));
+		assertEquals("{\"a\": true}", o("a", true).s);
 	}
 	
 	@Test
 	public void float_object() throws Exception {
-		assertEquals("{\"a\": 1.23}", J.o("a", 1.23));
+		assertEquals("{\"a\": 1.23}", o("a", 1.23).s);
 	}
 	
-	
+	@Test
+	public void nested_object() throws Exception {
+		assertEquals("{\"a\": {\"b\": \"c\"}}", o("a", o("b", "c")).s);
+	}
 }
